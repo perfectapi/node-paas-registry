20af884d0b60575adcca5cd3e8c5cadf5346f046
Steve Campbell <dukeytoo@gmail.com>
2012-02-07 19:31:26 -0600

    Work in progress - basic operations are working.  Need to tighten up code and add tests

---
+++ bin/registry.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+
+var redis = require("redis");
+var perfectapi = require('perfectapi');  
+//var perfectapi = require('../../perfectapi/api.js')
+var path = require('path');
+var main = require('../lib/main.js');
+
+var configPath = path.resolve(__dirname, '..', 'perfectapi.json');
+var parser = new perfectapi.Parser();
+
+//handle the commands
+parser.on("registerInstance", function(config, callback) {
+  var db = getRedisClient(config);
+  main.registerInstance(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+parser.on("unregisterInstance", function(config, callback) {
+  var db = getRedisClient(config);
+  main.unregisterInstance(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+parser.on("getServiceInfo", function(config, callback) {
+  var db = getRedisClient(config);
+  main.getServiceInfo(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+parser.on("listServices", function(config, callback) {
+  var db = getRedisClient(config);
+  main.listServices(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+parser.on("listUnclaimedInstances", function(config, callback) {
+  var db = getRedisClient(config);
+  main.listUnclaimedInstances(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+parser.on("claimInstance", function(config, callback) {
+  var db = getRedisClient(config);
+  main.claimInstance(db, config, function(err, result) {
+    db.quit();
+    callback(err, result);
+  });
+});
+
+module.exports = parser.parse(configPath);
+
+function getRedisClient(config) {
+  var db = redis.createClient(6379, config.environment.REDIS_HOST);
+  db.select(config.environment.REDIS_DB_INDEX);
+  db.on("error", function (err) {
+    console.log(err);
+  });
+  
+  return db;
+}
